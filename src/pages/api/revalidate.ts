9640c7e6c0af62a2495c9347698ae145c3552693
Napat Srichan <napatsrichan2001@gmail.com>
2022-10-19 16:36:04 -0700

    add on-demand isr

---
+++ src/pages/api/revalidate.ts
@@ -0,0 +1,33 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+const handler = async (
+  req: NextApiRequest,
+  res: NextApiResponse<{
+    code: number;
+    message: string;
+  }>
+) => {
+  const requestBody = req.body;
+  if (req.method === "POST")
+    if (requestBody !== undefined && requestBody !== null)
+      if (requestBody.SECRET !== undefined && requestBody.SECRET !== null)
+        if (requestBody.SECRET === process.env.SECRET)
+          try {
+            await res.revalidate("/dining");
+            await res.revalidate("/metro");
+            return res.status(200).json({
+              code: 200,
+              message: "Revalidation successful",
+            });
+          } catch {
+            return res.status(500).json({
+              code: 500,
+              message: "Revalidation failed",
+            });
+          }
+  return res.status(403).json({
+    code: 403,
+    message: "Forbidden",
+  });
+};
+export default handler;
