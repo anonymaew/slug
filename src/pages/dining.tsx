302d35ec1429ae9765452a818fe7312bf2dfec06
Napat Srichan <napatsrichan2001@gmail.com>
2022-10-24 16:29:30 -0700

    add dining datepicker

--- src/pages/dining.tsx
+++ src/pages/dining.tsx
@@ -1,19 +1,42 @@
-import Head from 'next/head';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+import { CalendarIcon } from '@heroicons/react/24/solid';
 
 import AllergenFilterModal from '../components/allergenFilterModal';
+import Calendar from '../components/calendar';
 import DiningMenu from '../components/diningMenu';
 import DiningSelector from '../components/diningSelector';
 import { AllergenFilter, DefaultAllergens } from '../interfaces/allergenFilter';
-import { DiningLists, Location } from '../interfaces/diningList';
+import { DiningLists, Meal } from '../interfaces/diningList';
 import MainLayout from '../layouts/main';
 import getDiningList from '../lib/getDiningList';
 
 const Page = (props: { data: DiningLists }) => {
-  const [location, setLocation] = useState<Location>(props.data.locations[0]);
+  const [diningList, setDiningList] = useState<DiningLists>(props.data);
+  const [location, setLocation] = useState<number>(0);
+  const [date, setDate] = useState<Date>(new Date());
+  const [meals, setMeals] = useState<Meal[]>(props.data.locations[0].meals);
   const [allergens, setAllergens] =
     useState<AllergenFilter[]>(DefaultAllergens);
   const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    setMeals(diningList.locations[location].meals);
+  }, [diningList, location]);
+
+  useEffect(() => {
+    if (date.toDateString() === new Date().toDateString())
+      setDiningList(props.data);
+    else
+      (async () => {
+        setLoading(true);
+        const result = await fetch(`/api/dining/list?date=${date.getTime()}`);
+        const data = await result.json();
+        setDiningList(data);
+        setLoading(false);
+      })();
+  }, [date]);
 
   return (
     <MainLayout>
@@ -21,31 +44,19 @@ const Page = (props: { data: DiningLists }) => {
         data={props.data}
         location={location}
         setLocation={setLocation}
+        setModalOpen={setModalOpen}
+        date={date}
+        setDate={setDate}
+        allergens={allergens}
       />
-      <div className="flex justify-between px-4 mt-4 text-base font-bold sm:text-lg text-amber-900 dark:text-amber-100">
-        <span>{location.date}</span>
-        <button
-          className="underline rounded-full focus-visible:outline focus-visible:outline-4 focus-visible:outline-offset-2 focus-visible:outline-amber-500 w-fit"
-          onClick={() => setModalOpen(true)}
-        >
-          {`Allergen filter${
-            allergens.filter((i) => i.checked).length > 0
-              ? ` (${allergens.filter((i) => i.checked).length})`
-              : ""
-          }`}
-        </button>
-      </div>
-      <DiningMenu meals={location.meals} filters={allergens} />
+      <div className="flex justify-between px-4 mt-4 text-base font-bold sm:text-lg text-amber-900 dark:text-amber-100"></div>
+      <DiningMenu meals={meals} filters={allergens} loading={loading} />
       <AllergenFilterModal
         modalOpen={modalOpen}
         setModalOpen={setModalOpen}
         allergens={allergens}
         onAllergenChange={setAllergens}
       />
-      <p>{`Latest built: ${new Date(props.data.updated).toLocaleString(
-        "en-US",
-        { timeZone: "America/Los_Angeles" }
-      )}`}</p>
     </MainLayout>
   );
 };
